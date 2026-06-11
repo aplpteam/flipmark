4d253a8d3eda452d83b60b78be17b1c4a0c04988
angelica tang <angelicatang07@gmail.com>
2026-06-10 22:22:00 -0400

    camera proportions

---
+++ build/runner/CMakeFiles/flipmark.dir/DependInfo.cmake
@@ -0,0 +1,55 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "RC"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_RC
+  "C:/projects/flipmark/flipmark/windows/runner/Runner.rc" "C:/projects/flipmark/build/runner/CMakeFiles/flipmark.dir/Runner.rc.obj"
+  )
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_RC
+  "FLUTTER_VERSION=\"1.0.0+1\""
+  "FLUTTER_VERSION_BUILD=1"
+  "FLUTTER_VERSION_MAJOR=1"
+  "FLUTTER_VERSION_MINOR=0"
+  "FLUTTER_VERSION_PATCH=0"
+  "NOMINMAX"
+  "UNICODE"
+  "_DEBUG"
+  "_HAS_EXCEPTIONS=0"
+  "_UNICODE"
+  )
+
+# The include file search paths:
+set(CMAKE_RC_TARGET_INCLUDE_PATH
+  "C:/projects/flipmark/flipmark/windows"
+  "C:/projects/flipmark/flipmark/windows/flutter/ephemeral"
+  "C:/projects/flipmark/flipmark/windows/flutter/ephemeral/cpp_client_wrapper/include"
+  "C:/projects/flipmark/flipmark/windows/flutter/ephemeral/.plugin_symlinks/camera_windows/windows/include"
+  "C:/projects/flipmark/flipmark/windows/flutter/ephemeral/.plugin_symlinks/file_selector_windows/windows/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/projects/flipmark/flipmark/windows/flutter/generated_plugin_registrant.cc" "runner/CMakeFiles/flipmark.dir/__/flutter/generated_plugin_registrant.cc.obj" "gcc" "runner/CMakeFiles/flipmark.dir/__/flutter/generated_plugin_registrant.cc.obj.d"
+  "C:/projects/flipmark/flipmark/windows/runner/flutter_window.cpp" "runner/CMakeFiles/flipmark.dir/flutter_window.cpp.obj" "gcc" "runner/CMakeFiles/flipmark.dir/flutter_window.cpp.obj.d"
+  "C:/projects/flipmark/flipmark/windows/runner/main.cpp" "runner/CMakeFiles/flipmark.dir/main.cpp.obj" "gcc" "runner/CMakeFiles/flipmark.dir/main.cpp.obj.d"
+  "C:/projects/flipmark/flipmark/windows/runner/utils.cpp" "runner/CMakeFiles/flipmark.dir/utils.cpp.obj" "gcc" "runner/CMakeFiles/flipmark.dir/utils.cpp.obj.d"
+  "C:/projects/flipmark/flipmark/windows/runner/win32_window.cpp" "runner/CMakeFiles/flipmark.dir/win32_window.cpp.obj" "gcc" "runner/CMakeFiles/flipmark.dir/win32_window.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
